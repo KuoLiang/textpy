b5e19305800e39ab18953a87460d8ee4a08c232e
Kuo-Liang Ou <kloutw@gmail.com>
2021-11-22 16:51:14 +0800

    a

---
+++ read_json.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 22 16:46:04 2021
+
+@author: klout
+"""
+
+import json
+from pprint import pprint
+with open
---
+++ tuple.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 22 11:12:20 2021
+
+@author: kloutw
+"""
+
+a = [1,2,3,4,5,6,7,8,9,10]
+b = [1,2,3,4,5,"6","7","8","9","10"]
+c = [a,b]
+
+print(a)
+print(b)
+print(c)
+
+print(a.sort())
+print(a.sort(reverse=1))
+
+d=(1,2,3,4)
